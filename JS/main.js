b3190c8927b9ba851a69198d4aaffdbf76b2bcc1
LINUXh0flod <gabriel32bruninha@gmail.com>
2022-09-16 23:53:42 -0300

    Adicionando function no body

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,16 @@
 var menuLogo = window.document.getElementById('burguer') 
 var menuItens = window.document.getElementById('headei')
+
+function mudouTamnho() {
+  if (window.innerWidth >= 900) {
+    menuItens.style.display = 'block'
+  } else {
+    menuItens.style.display = 'none'
+  }
+}
+
+
+
 var s1 = window.document.getElementById('sec1')
 function clicar() {
 if (menuItens.style.display == 'block') {
